6bad5f9e681659627b6d8659ae3efa550e6e41cf
Priyankag0704 <126230404+Priyankag0704@users.noreply.github.com>
2024-03-11 10:29:45 +0530

    Add files via upload

---
+++ Quiz-App/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        question: "Which device is required for the Internet connection?",
+        option1: "Modem",
+        option2: "Router",
+        option3: "LAN Cable",
+        option4: "Pen Drive",
+        ans: 1,
+    },
+    {
+        question: "Which of the following will not advance the slide in the slide show view?",
+        option1: "Esc Key",
+        option2: "Enter Key",
+        option3: "Space bar",
+        option4: "Mouse Button",
+        ans: 1,
+    },
+    {
+        question: "Which of the following is not an image/graphic file format?",
+        option1: "PNG",
+        option2: "GIF",
+        option3: "BMP",
+        option4: "GUI",
+        ans: 4,
+    },
+    {
+        question: "How is the resolution of the screen shown?",
+        option1: "Dots",
+        option2: "Colors",
+        option3: "Pixels per inch",
+        option4: "Dot per inch",
+        ans: 3,
+    },
+    {
+        question: "What does the comment give for a cell entry to show on the screen?",
+        option1: "Comment",
+        option2: "Indicator",
+        option3: "Picture",
+        option4: "None of these",
+        ans: 1,
+    }
+]
